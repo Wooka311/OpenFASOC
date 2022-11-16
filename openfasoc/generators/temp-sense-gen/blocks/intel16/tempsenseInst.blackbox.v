20f75b92c73266ee2dc2ddd32fd1d7cc227825a0
Ashbir Aviat Fadila <afadila@umich.edu>
2022-11-16 10:56:12 -0500

    Add headerA, headerB, and headerC

--- openfasoc/generators/temp-sense-gen/blocks/intel16/tempsenseInst.blackbox.v
+++ openfasoc/generators/temp-sense-gen/blocks/intel16/tempsenseInst.blackbox.v
@@ -10,3 +10,18 @@ endmodule
 );
 parameter dont_touch = "on";
 endmodule
+(* blackbox *) module headerA(
+  inout VIN
+);
+parameter dont_touch = "on";
+endmodule
+(* blackbox *) module headerB(
+  inout VIN
+);
+parameter dont_touch = "on";
+endmodule
+(* blackbox *) module headerC(
+  inout VIN
+);
+parameter dont_touch = "on";
+endmodule
