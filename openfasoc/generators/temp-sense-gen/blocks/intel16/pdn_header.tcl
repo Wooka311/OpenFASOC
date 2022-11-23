66872cef5f66cfebbb8572db2bd075883b9fd895
Ashbir Aviat Fadila <afadila@umich.edu>
2022-11-23 03:13:43 -0500

    seperate header placement

---
+++ openfasoc/generators/temp-sense-gen/blocks/intel16/pdn_header.tcl
@@ -0,0 +1,4 @@
+place_cell -cell headerA -inst temp_analog_1.a_header_0 -origin {16.308 10.71} -orient R0 -status FIRM
+place_cell -cell headerA -inst temp_analog_1.a_header_1 -origin {16.308 13.23} -orient R0 -status FIRM
+place_cell -cell headerA -inst temp_analog_1.a_header_2 -origin {16.308 11.97} -orient R0 -status FIRM
+
